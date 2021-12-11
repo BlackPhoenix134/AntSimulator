8153d94ad0e90abfed9a84f39e9ae866d2f53e7d
BlackPhoenix134 <46650884+BlackPhoenix134@users.noreply.github.com>
2021-12-11 12:36:03 +0100

    SAVEPOINT

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,53 @@
+newoption 
+{
+   trigger = "opengl43",
+   description = "use OpenGL 4.3"
+}
+
 workspace "AntSimulator"
 	architecture "x64"
 
 	configurations 
 	{
 		"Debug",
-		"Release",
-		"Dist"
-	}
-
-	platforms 
-	{ 
-		"Win64"
+		"Release"
 	}
 
 	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "raylib"
+		filter "configurations:Debug OR Release"
+			kind "StaticLib"
+			
+		filter "action:vs*"
+			defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS"}
+			characterset ("MBCS")
+		
+		filter "system:windows"
+			defines{"_WIN32"}
+			links {"winmm", "kernel32", "opengl32", "kernel32", "gdi32"}
+		
+		filter{}
+		
+		defines{"PLATFORM_DESKTOP"}
+		if (_OPTIONS["opengl43"]) then
+			defines{"GRAPHICS_API_OPENGL_43"}
+		else
+			defines{"GRAPHICS_API_OPENGL_33"}
+		end
+	
+		location "vendor/raylib"
+		language "C"
+		targetdir ("bin/" .. outputdir .. "/AntSimulator")	
+		
+		includedirs { "vendor/raylib/src", "vendor/raylib/src/external/glfw/include"}
+		vpaths 
+		{
+			["Header Files"] = { "vendor/raylib/src/**.h"},
+			["Source Files/*"] = {"vendor/raylib/src/**.c"},
+		}
+		files {"raylib/src/*.h", "vendor/raylib/src/*.c"}
+
 
 project "AntSimulator"
 	location "AntSimulator"
@@ -37,7 +71,7 @@ project "AntSimulator"
 
 
 
-	filter "platforms:Win64"
+	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "10.0.19041.0"
@@ -48,8 +82,4 @@ project "AntSimulator"
 
 	filter "configurations:Release"
 		defines { "AS_RELEASE" }
-		symbols "On"
-
-	filter "configurations:Dist"
-		defines { "AS_DIST" }
 		symbols "On"
